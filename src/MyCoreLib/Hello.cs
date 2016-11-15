39b8fe3cc126f5e8e39c993c8ff028f896e999de
unknown <shucai@microsoft.com>
2016-11-15 14:22:34 -0800

    first commit

---
+++ src/MyCoreLib/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoreLib
+{
+    public static class Hello
+    {
+        public static string SayHello(string name) => $"Hello {name}!";
+    }
+}
